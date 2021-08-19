9175f70d4ed051dca6df5ab1b937fd10eecab0f2
joshuaboud <joshuaboud@hotmail.com>
2021-08-19 13:16:49 -0300

    remove LED number output

--- simulate/print_buf.c
+++ simulate/print_buf.c
@@ -10,13 +10,13 @@ void ws2812_setleds (struct cRGB  *ledarray, uint16_t number_of_leds) {
         for (int j = 0; j < FRAME_W; j++) {
             int buff_ind = i * FRAME_W + j;
             struct cRGB pixel = ledarray[buff_ind];
-            printf("%2d#%02x%02x%02x,", buff_ind, pixel.r, pixel.b, pixel.g);
+            printf("#%02x%02x%02x,", pixel.r, pixel.b, pixel.g);
         }
         putchar(' ');
         for (int j = 0; j < FRAME_W; j++) {
             int buff_ind = i * FRAME_W + j + FRAME_PIXELS;
             struct cRGB pixel = ledarray[buff_ind];
-            printf("%2d#%02x%02x%02x,", buff_ind, pixel.r, pixel.b, pixel.g);
+            printf("d#%02x%02x%02x,", pixel.r, pixel.b, pixel.g);
         }
         putchar('\n');
     }
