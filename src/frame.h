cad53126550d146230352dfadd5778e5ea0f3614
joshuaboud <joshuaboud@hotmail.com>
2021-08-19 13:14:11 -0300

    change RGB6 to be union of bf and uint8_t

--- src/frame.h
+++ src/frame.h
@@ -1,16 +1,24 @@
 #pragma once
 
+#ifdef SIMULATE
+#include <stdint.h>
+#endif
+
 #define FRAME_W 5
 #define FRAME_H 6
+#define FRAME_PIXELS (FRAME_W * FRAME_H)
 
 #define RGB6_2_cRGB(x) {x.g << 6, x.r << 6, x.b << 6}
 
 enum FrameType { COPY, MIRROR, LEFT, RIGHT };
 
-typedef struct {
-    uint8_t r : 2;
-    uint8_t g : 2;
-    uint8_t b : 2;
+typedef union {
+    uint8_t all;
+    struct {
+        uint8_t r : 2;
+        uint8_t g : 2;
+        uint8_t b : 2;
+    };
 } RGB6;
 
 typedef struct {
