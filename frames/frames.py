fe0217b325ff01805d6b9484f647056704f94e85
joshuaboud <joshuaboud@hotmail.com>
2021-08-20 16:30:21 -0300

    add program to draw emotes

---
+++ frames/frames.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_n,
+    KEYDOWN,
+    QUIT,
+    MOUSEBUTTONUP
+)
+
+SCREEN_W = 640
+SCREEN_H = 480
+
+FRAME_LEFT = 20
+FRAME_TOP = 20
+FRAME_W_REAL = 5
+FRAME_H_REAL = 6
+FRAME_SCALE = 50
+FRAME_W = FRAME_W_REAL * FRAME_SCALE
+FRAME_H = FRAME_H_REAL * FRAME_SCALE
+
+PALETTE_ROWS = 16
+NUM_COLOUR = pow(4, 3)
+PALETTE_BLOCK_SZ = 20
+PALETTE_START = (FRAME_LEFT + FRAME_W + 20, 20)
+PALETTE_END = (PALETTE_START[0] + NUM_COLOUR/PALETTE_ROWS*PALETTE_BLOCK_SZ, PALETTE_START[1] + PALETTE_BLOCK_SZ*PALETTE_ROWS)
+PALETTE_BOUND = (PALETTE_START, PALETTE_END)
+
+TYPE_START = (20, FRAME_TOP + FRAME_H + 20)
+TYPE_END = (TYPE_START[0] + 20, TYPE_START[1] + 20)
+
+BG = (20,20,20)
+
+FRAME_TYPES = ["COPY", "MIRROR", "LEFT", "RIGHT"]
+
+current_emote = 0
+current_frame = 0
+current_colour = (0,0,0)
+
+def new_frame(copy_frame = None):
+    info = {
+        "duration": 1,
+        "last_frame": 1,
+        "type": 0
+    }
+    pixels = []
+    for i in range(FRAME_H_REAL):
+        pixels.append([])
+        for j in range(FRAME_W_REAL):
+            if copy_frame is None:
+                pixels[i].append((0,0,0))
+            else:
+                pixels[i].append(copy_frame["pixels"][i][j])
+    return {"info": info, "pixels": pixels}
+
+def clicked_canvas(pos):
+    return pos[0] >= FRAME_LEFT and pos[0] < FRAME_LEFT + FRAME_W and pos[1] >= FRAME_TOP and pos[1] < FRAME_TOP + FRAME_H
+
+def clicked_palette(pos):
+    return pos[0] >= PALETTE_START[0] and pos[1] >= PALETTE_START[1] and pos[0] < PALETTE_END[0] and pos[1] < PALETTE_END[1]
+
+def clicked_type(pos):
+    return pos[0] >= TYPE_START[0] and pos[1] >= TYPE_START[1] and pos[0] < TYPE_END[0] and pos[1] < TYPE_END[1]
+
+def gen_palette(screen):
+    ind = 0
+    top = PALETTE_START[1]
+    for r in range(4):
+        for g in range(4):
+            for b in range(4):
+                screen.fill((r << 6, g << 6, b << 6), (
+                    (PALETTE_START[0] + ind*PALETTE_BLOCK_SZ, top),
+                    (PALETTE_BLOCK_SZ, PALETTE_BLOCK_SZ)
+                ))
+                ind = (ind + 1) % (NUM_COLOUR / PALETTE_ROWS)
+                if ind == 0:
+                    top += PALETTE_BLOCK_SZ
+
+def change_colour(screen, pos):
+    global current_colour
+    current_colour = screen.get_at(pos)
+
+def draw_screen(screen, text, emotes):
+    global TYPE_END
+    frame = emotes[current_emote][current_frame]["pixels"]
+    screen.fill(BG)
+    for i in range(len(frame)):
+        for j in range(len(frame[i])):
+            colour = frame[i][j]
+            screen.fill(colour, pygame.Rect(FRAME_LEFT + j*FRAME_SCALE, FRAME_TOP + i*FRAME_SCALE, FRAME_SCALE, FRAME_SCALE))
+    textsurface = text.render(
+        f'Emote: {current_emote + 1}/{len(emotes)} Frame: {current_frame + 1}/{len(emotes[current_emote])}',
+        False, (255,255,255)
+    )
+    screen.blit(textsurface, (20, SCREEN_H - 20))
+    textsurface = text.render(
+        f'Type: {FRAME_TYPES[emotes[current_emote][current_frame]["info"]["type"]]}',
+        False, (255,255,255)
+    )
+    TYPE_END = (TYPE_START[0] + textsurface.get_width(), TYPE_START[1] + textsurface.get_height())
+    screen.blit(textsurface, TYPE_START)
+    gen_palette(screen)
+    pygame.display.flip()
+
+def colour_pixel(frame, pos):
+    global current_colour
+    x, y = (pos[0] - FRAME_LEFT)//FRAME_SCALE, (pos[1] - FRAME_TOP)//FRAME_SCALE
+    if x >= 0 and x < FRAME_W_REAL and y >= 0 and y < FRAME_H_REAL:
+        frame[y][x] = current_colour
+
+def save(emotes):
+    f = open(sys.argv[1], "w")
+    emote_ind = 0
+    emote_inds = []
+    for emote in emotes:
+        emote_inds.append(emote_ind)
+        f.write("{\n")
+        for frame in emote:
+            f.write("\t{\n")
+            for row in frame["pixels"]:
+                f.write("\t\t\t{")
+                for col in row:
+                    f.write('{{0b{0:06b}}}, '.format(col[0] >> 2 | col[1] >> 4 | col[2] >> 6))
+                f.write("},\n")
+            emote_ind += 1
+            f.write("\t},\n")
+        f.write("},\n")
+    f.close()
+
+def setup():
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_W, SCREEN_H])
+    pygame.font.init()
+    text = pygame.font.SysFont('monospace', 12)
+    return (screen, text)
+
+def loop(screen, text):
+    global current_emote
+    global current_frame
+    running = True
+    emotes = [[new_frame()] for i in range(9)]
+    once = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                elif event.key == K_UP:
+                    current_emote = max([current_emote - 1, 0])
+                    current_frame = min([current_frame, len(emotes[current_emote]) - 1])
+                elif event.key == K_DOWN:
+                    current_emote = min([current_emote + 1, len(emotes) - 1])
+                    current_frame = min([current_frame, len(emotes[current_emote]) - 1])
+                elif event.key == K_LEFT:
+                    current_frame = max([current_frame - 1, 0])
+                elif event.key == K_RIGHT:
+                    current_frame = min([current_frame + 1, len(emotes[current_emote]) - 1])
+                elif event.key == K_n:
+                    emotes[current_emote][len(emotes[current_emote]) - 1]["info"]["last_frame"] = 0
+                    emotes[current_emote].append(new_frame(emotes[current_emote][current_frame]))
+                    emotes[current_emote][len(emotes[current_emote]) - 1]["info"]["last_frame"] = 1
+        pos = pygame.mouse.get_pos()
+        pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
+        if pressed1:
+            if clicked_canvas(pos):
+                colour_pixel(emotes[current_emote][current_frame]["pixels"], pos)
+            elif clicked_palette(pos):
+                change_colour(screen, pos)
+            elif clicked_type(pos) and once:
+                emotes[current_emote][current_frame]["info"]["type"] = (emotes[current_emote][current_frame]["info"]["type"] + 1) % len(FRAME_TYPES)
+            once = False
+        else:
+            once = True
+        draw_screen(screen, text, emotes)
+    save(emotes)
+
+def main():
+    screen, text = setup()
+    loop(screen, text)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
